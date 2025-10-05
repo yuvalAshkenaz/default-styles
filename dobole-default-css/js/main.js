127eda00fa81a98251a0e0bcf91fd69ca941563b
benharari48 <ben@dooble.co.il>
2025-10-05 16:02:37 +0300

    Remove dobole-default-css

--- dobole-default-css/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initModals } from './components/modal.js';
-import { initTooltips } from './components/tooltips.js';
-import { initAlerts } from './components/alerts.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initModals();
-  initTooltips();
-  initAlerts();
-});
